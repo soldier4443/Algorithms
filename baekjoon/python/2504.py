fa62c2ff567103d450b1e98bc257a080ac3eaa48
tura <soldier4443@gmail.com>
2018-10-21 13:55:53 +0900

    2504번 문제 품

--- baekjoon/python/2504.py
+++ baekjoon/python/2504.py
@@ -1,24 +1,25 @@
-ps = raw_input()
+ps = input()
 if len(ps) % 2 == 1:
   print(0)
 else:
-  s = 0
-  d = 0
   stack = []
-  r = [0] * 16
+  r = [0] * 30
   for p in ps:
+    co = 0
     if p == ')' and stack and stack[-1] == '(':
-      stack.pop()
-      r[len(stack)] += 2
+      co = 2
     elif p == ']' and stack and stack[-1] == '[':
+      co = 3
+
+    if co:
       stack.pop()
-      r[len(stack)] += 3
-    
+      l = len(stack)
+      up = 1 if r[l+1] == 0 else r[l+1]
+      r[l+1] = 0
+      r[l] += co * up
     else:
       stack.append(p)
-  print(s)
-
-  # (()[[]])([])
-  # 2
-  # 22
-  # 
+  if len(stack) == 0:
+    print(r[0])
+  else:
+    print(0)
